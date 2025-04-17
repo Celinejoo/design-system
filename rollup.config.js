1d69ca86d799f8a05adc107aadcf101bf8e3602f
hyunjungjoo <joohj0509@gmail.com>
2025-04-18 00:41:56 +0900

    초기 프로젝트 세팅: 디자인 시스템 라이브러리

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+const peerDepsExternal = require("rollup-plugin-peer-deps-external");
+const resolve = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+const typescript = require("@rollup/plugin-typescript");
+const postcss = require("rollup-plugin-postcss");
+
+module.exports = {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.cjs.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: "./tsconfig.json" }),
+    postcss({
+      modules: true,
+      extract: true,
+      minimize: true,
+      sourceMap: true,
+    }),
+  ],
+};
