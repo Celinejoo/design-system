707eb222111b4415069f998a07a083f5112741ed
hyunjungjoo <joohj0509@gmail.com>
2025-04-18 16:40:51 +0900

    린트 수정

--- src/styles/utils/generateCssVariables.ts
+++ src/styles/utils/generateCssVariables.ts
@@ -4,15 +4,15 @@ export const generateCssVariables = (
 ) => {
   let result = "";
 
-  for (const [key, value] of Object.entries(obj)) {
+  Object.entries(obj).forEach(([key, value]) => {
     if (typeof value === "object" && value !== null) {
-      for (const [shade, shadeValue] of Object.entries(value)) {
+      Object.entries(value).forEach(([shade, shadeValue]) => {
         result += `${prefix}-${key}-${shade}: ${shadeValue};\n`;
-      }
+      });
     } else {
       result += `${prefix}-${key}: ${value};\n`;
     }
-  }
+  });
 
   return result;
 };
