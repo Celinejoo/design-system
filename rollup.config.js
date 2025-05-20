c82a753ae23495f77a1989159d56ac61a7615b31
hyunjungjoo <joohj0509@gmail.com>
2025-05-20 18:29:16 +0900

    fix: rollup fix

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ const resolve = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
 const typescript = require("@rollup/plugin-typescript");
 const postcss = require("rollup-plugin-postcss");
-const copy = require("rollup-plugin-copy");
 const dts = require("rollup-plugin-dts").default;
 const postcssCustomProperties = require("postcss-custom-properties");
 
@@ -37,20 +36,6 @@ module.exports = [
           postcssCustomProperties({
             preserve: false,
           }),
-        ],
-      }),
-      copy({
-        targets: [
-          {
-            src: "src/styles/**/*",
-            dest: "dist/styles",
-            flatten: false,
-          },
-          {
-            src: "src/components/**/*.{scss,css}",
-            dest: "dist/components",
-            flatten: false,
-          },
         ],
       }),
     ],
