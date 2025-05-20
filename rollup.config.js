9a832147e7e722fdfff93167a6efcb27f2c9b61d
hyunjungjoo <joohj0509@gmail.com>
2025-05-20 18:13:29 +0900

    fix : rollup 수정

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,9 @@ const resolve = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
 const typescript = require("@rollup/plugin-typescript");
 const postcss = require("rollup-plugin-postcss");
+const copy = require("rollup-plugin-copy");
 const dts = require("rollup-plugin-dts").default;
+const postcssCustomProperties = require("postcss-custom-properties");
 
 module.exports = [
   {
@@ -27,10 +29,27 @@ module.exports = [
       typescript({ tsconfig: "./tsconfig.json" }),
       postcss({
         modules: true,
-        extract: true,
+        extract: "index.css", // dist/index.css 로 추출됨
         minimize: true,
         sourceMap: true,
         use: ["sass"],
+        plugins: [
+          postcssCustomProperties(), // ⚠️ var(--xxx) → 값으로 변환
+        ],
+      }),
+      copy({
+        targets: [
+          {
+            src: "src/styles/**/*",
+            dest: "dist/styles",
+            flatten: false,
+          },
+          {
+            src: "src/components/**/*.{scss,css}",
+            dest: "dist/components",
+            flatten: false,
+          },
+        ],
       }),
     ],
   },
