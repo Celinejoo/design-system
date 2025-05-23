ae83b165464e74796b46d9f9e1bc1b8451c4c92b
hyunjungjoo <joohj0509@gmail.com>
2025-05-23 18:47:43 +0900

    fix: close button style, lint

--- src/components/CloseButton/CloseButton.tsx
+++ src/components/CloseButton/CloseButton.tsx
@@ -1,19 +1,26 @@
 import { CloseButtonProps } from "./CloseButton.types";
 import styles from "./CloseButton.module.scss";
 import { cx } from "../../utils/cx";
+
 export const CloseButton = ({ onClick, size }: CloseButtonProps) => {
+  let width;
+  if (size === "sm") {
+    width = "14";
+  } else if (size === "md") {
+    width = "16";
+  } else {
+    width = "20";
+  }
+
   return (
     <button type="button" aria-label="Close" onClick={onClick}>
-      <svg
-        viewBox="0 0 24 24"
-        width={size === "sm" ? "14" : size === "md" ? "18" : 20}
-      >
+      <svg viewBox="0 0 24 24" width={width}>
         <path
           className={cx(styles.close)}
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M18.7071 6.70711C19.0976 6.31658 19.0976 5.68342 18.7071 5.29289C18.3166 4.90237 17.6834 4.90237 17.2929 5.29289L12 10.5858L6.70711 5.29289C6.31658 4.90237 5.68342 4.90237 5.29289 5.29289C4.90237 5.68342 4.90237 6.31658 5.29289 6.70711L10.5858 12L5.29289 17.2929C4.90237 17.6834 4.90237 18.3166 5.29289 18.7071C5.68342 19.0976 6.31658 19.0976 6.70711 18.7071L12 13.4142L17.2929 18.7071C17.6834 19.0976 18.3166 19.0976 18.7071 18.7071C19.0976 18.3166 19.0976 17.6834 18.7071 17.2929L13.4142 12L18.7071 6.70711Z"
-        ></path>
+        />
       </svg>
     </button>
   );
